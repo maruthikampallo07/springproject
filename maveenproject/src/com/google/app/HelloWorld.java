465809c21049a72ffdbd6a848f53bcdcfbfcaa49
hp <hp@DESKTOP-GCPCRBC>
2021-05-05 11:26:01 +0530

    Comments are added into the hello world program just see and chek it

--- maveenproject/src/com/google/app/HelloWorld.java
+++ maveenproject/src/com/google/app/HelloWorld.java
@@ -3,7 +3,7 @@ package com.google.app;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
+		//comments add in the app package
 		System.out.println("hello world two");
 	}
 }
